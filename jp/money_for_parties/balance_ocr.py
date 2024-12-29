d1ca1d7a66f18ed9f4795b4b93e0a901c5c69313
isamu-isozaki <isamu.website@gmail.com>
2024-12-29 02:01:06 -0500

    Raise exception

--- jp/money_for_parties/balance_ocr.py
+++ jp/money_for_parties/balance_ocr.py
@@ -388,7 +388,8 @@ def get_content(image_url, temperature=0.5, debug=False, max_num_retries=3):
                 "guided_choice": ["あります", "ありません"]
             }) == "あります"
             if not index_exists:
-                raise Exception("Index out of bounds")
+              raise Exception("Index did not exist")
+              return {"failed": True}
 
             index = int(get_answer2question(base64_image, "上の’（その’で始まる箇所の数字を出力してください。数字のみを出力してください。", {
                 "guided_regex": "[0-9]+"
@@ -416,8 +417,9 @@ def get_content(image_url, temperature=0.5, debug=False, max_num_retries=3):
                 "failed": False
             }
             if handwritten or handwritten_corrected:
-                raise Exception("handwritten")
-
+              raise Exception("handwritten")
+              output["failed"] = True
+              return output
             for question in questions:
                 while True:
                     try:
