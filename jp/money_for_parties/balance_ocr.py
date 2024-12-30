370a98c03e2be9df9659eb34b4346688279e1925
isamu-isozaki <isamu.website@gmail.com>
2024-12-30 17:49:01 -0500

    Remove n/a values

--- jp/money_for_parties/balance_ocr.py
+++ jp/money_for_parties/balance_ocr.py
@@ -436,8 +436,7 @@ def get_content(image_url, temperature=0.5, debug=False, max_num_retries=3):
                             temp_path = StringIO(chat_completion)
                             data = pd.read_csv(temp_path, sep=",")
                             if data.isnull().values.any():
-                                print(data)
-                                raise Exception("Got nan values in pandas")
+                                data = data.replace(pd.NA, 'N/A')
                             data = data.to_dict()
                             data_output = {}
                             for key in data:
